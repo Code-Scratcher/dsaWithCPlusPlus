7796b6c6cb61d533a7e13a628f52c0cd6a3412bb
DEBOPRIYO DEY <77635358+Code-Scratcher@users.noreply.github.com>
2023-02-27 18:40:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int forloop(int number)
+{
+    int reverse=0;
+    for(;number!=0;number=number/10)
+    {
+        int rem=number%10;
+        
+        reverse=reverse*10+rem;
+    }
+    
+    return reverse;
+}
+
+int whileloop(int number)
+{
+    int rem;
+    int reverse=0;
+    
+    while(number>0)
+    { 
+        rem=number%10;
+        reverse=reverse*10+rem;
+        number=number/10;
+    }
+   return reverse;
+}
+
+int dowhileloop(int number)
+{
+  int rem;
+  int reverse=0;
+  do
+    {
+      rem=number%10;
+      reverse=reverse*10+rem;
+      number=number/10;
+    }
+    while(number>0);
+    return reverse;
+}
+
+
+int main()
+{
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    cout<<"Reverse using for loop:"<<forloop(n)<<"\n";
+    cout<<"Reverse using while loop:"<<whileloop(n)<<"\n";
+    cout<<"Reverse using while loop:"<<dowhileloop(n);
+  
+
+    return 0;
+}
